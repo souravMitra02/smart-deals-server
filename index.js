883f973bf7b36707d89fc97ed2f06ebbd2785740
Analytics Dashboard <developer@analytics-dashboard.local>
2026-04-14 12:17:16 +0600

    added post method

---
+++ index.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 3000;
+
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yiklsv0.mongodb.net/?appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("smart_db").collection("products");
+
+    app.post("/products", async (req, res) => {
+      const data = req.body;
+      const result = await db.insertOne(data);
+      console.log(result);
+      res.send(result);
+
+  })
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('smart deals server')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
