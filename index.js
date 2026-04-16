1b25b156b7cf8d80e9f135edf3f96365c2822421
Analytics Dashboard <developer@analytics-dashboard.local>
2026-04-16 09:53:43 +0600

    refactor: clean up code formatting and improve readability

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-require('dotenv').config();
-const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('cors');
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const app = express();
 const port = process.env.PORT || 3000;
 
-
 // middleware
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yiklsv0.mongodb.net/?appName=Cluster0`;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
-
 
 async function run() {
   try {
@@ -35,30 +30,43 @@ async function run() {
       const result = await db.insertOne(data);
       console.log(result);
       res.send(result);
+    });
 
-  })
+    app.patch("/products/:id", async (req, res) => {
+      const id = req.params.id;
+       const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateProducts = {
+        $set: {
+        name : data.name
+        },
+      };
+      const result = await db.updateOne(filter, updateProducts);
+      res.send(result);
+    });
 
-
-
-
-
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await db.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
-
     // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('smart deals server')
-})
+app.get("/", (req, res) => {
+  res.send("smart deals server");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
