db603a1022007c4f81628c080e836e2becdde943
Analytics Dashboard <developer@analytics-dashboard.local>
2026-04-17 13:23:58 +0600

    refactor: separate database collections for products and bids in API endpoints

--- index.js
+++ index.js
@@ -23,9 +23,11 @@ async function run() {
   try {
     await client.connect();
 
-    const db = client.db("smart_db").collection("products");
+    const db = client.db("smart_db");
+    const productsCollection = db.collection("products");
+    const bidsCollection = db.collection("bids");
 
-    app.get('/products', async (req, res) => {
+    app.get("/products", async (req, res) => {
       // const projectFields = {_id : 0 ,email: 1 };
       console.log(req.query);
       const email = req.query.email;
@@ -33,39 +35,36 @@ async function run() {
       if (email) {
         query.email = email;
       }
-      const cursor = db.find(query)
-        // .project(projectFields).sort({ price_min: 1 }).limit(4);
+      const cursor = productsCollection.find(query);
+      // .project(projectFields).sort({ price_min: 1 }).limit(4);
       const result = await cursor.toArray();
-      res.send(result)
-    })
-    
-    app.get("/products/:id", async(req, res) => {
+      res.send(result);
+    });
+
+    app.get("/products/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await db.findOne(query);
-      res.send(result)
-    })
-
-
-
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
 
     app.post("/products", async (req, res) => {
       const data = req.body;
-      const result = await db.insertOne(data);
+      const result = await productsCollection.insertOne(data);
       console.log(result);
       res.send(result);
     });
 
     app.patch("/products/:id", async (req, res) => {
       const id = req.params.id;
-       const data = req.body;
+      const data = req.body;
       const filter = { _id: new ObjectId(id) };
       const updateProducts = {
         $set: {
-        name : data.name
+          name: data.name,
         },
       };
-      const result = await db.updateOne(filter, updateProducts);
+      const result = await productsCollection.updateOne(filter, updateProducts);
       res.send(result);
     });
 
@@ -73,9 +72,29 @@ async function run() {
       const id = req.params.id;
       console.log(id);
       const query = { _id: new ObjectId(id) };
-      const result = await db.deleteOne(query);
+      const result = await productsCollection.deleteOne(query);
       res.send(result);
     });
+
+    // bids api
+
+    app.get("/bids", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.buyer_email = email;
+      }
+      const cursor = bidsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post('/bids', async(req, res) => {
+      const newBidsData = req.body;
+      const result = await bidsCollection.insertOne(newBidsData);
+      res.send(result)
+})
+
 
     await client.db("admin").command({ ping: 1 });
     console.log(
